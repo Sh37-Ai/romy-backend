ac5581f9a860e46a705bfc193fc47e7ec0ba80b4
Sh37-Ai <salahhakkal50@gmail.com>
2025-09-24 21:58:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -51,5 +51,9 @@ def predict():
 def healthz():
     return "OK", 200
 
+@app.route("/")
+def home():
+    return "Hello Render!", 200
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
