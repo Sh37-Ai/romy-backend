4b934cdb97259c6b6e9af4962778a38e79804968
Sh37-Ai <salahhakkal50@gmail.com>
2025-09-24 23:25:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -78,5 +78,7 @@ def predict():
 def healthz():
     return "OK", 200
 
+# --- Démarrage du serveur ---
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    port = int(os.environ.get("PORT", 8000))  # Railway fournit le port automatiquement
+    app.run(debug=True, host="0.0.0.0", port=port)
